0caa8ecd6327000c95baa98a0cfbcb0d9a972f6d
RomanVVVVV <romanvitvitskyjr@gmail.com>
2023-03-31 13:35:25 +0300

    Add project files.

---
+++ B23 Ex01 Roman 324065184 Ori 206589855/Ex01_01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex01_01
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter 3 binary numbers, each one 8 bits in length:");
+
+        }
+    }
+}

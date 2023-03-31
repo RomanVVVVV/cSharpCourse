3a379dc15d5bb6d15d60eeeca8619f20b267bdef
RomanVVVVV <romanvitvitskyjr@gmail.com>
2023-03-31 22:35:17 +0300

    completed parts 2 and 3 of ex1

---
+++ B23 Ex01 Roman 324065184 Ori 206589855/Ex01_02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace Ex01_02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int lengthOfSquare = 9;
+            int lineNumber = 1;
+
+            printTiltedSquare(lineNumber, lengthOfSquare);
+            Console.ReadLine();
+        }
+
+        public static void printTiltedSquare(int i_LineNumber, int i_LengthOfSquare)
+        {
+            int lineLength=0;
+            bool v_thisIsTheEnd = false;
+
+            if(i_LineNumber > i_LengthOfSquare)
+            {
+                v_thisIsTheEnd = true;
+            }
+
+            if(v_thisIsTheEnd == false)
+            {
+                if(i_LineNumber <= (i_LengthOfSquare / 2) + 1)
+                {
+                    lineLength = i_LineNumber * 2 - 1;
+                }
+                else
+                {
+                    lineLength = (i_LengthOfSquare - i_LineNumber) * 2 + 1;
+                }
+            }
+
+            for (int i = 0; i < (i_LengthOfSquare-lineLength)/2 ; i++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int i = 0; i < lineLength; i++)
+            {
+                Console.Write("*");
+            }
+
+            if(v_thisIsTheEnd == false)
+            {
+                Console.Write("\n");
+                printTiltedSquare(i_LineNumber+1,i_LengthOfSquare);
+            }
+
+                
+        }
+
+
+
+
+        
+    }
+}
---
+++ B23 Ex01 Roman 324065184 Ori 206589855/Ex01_03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex01_03
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int squareLength = 0;
+
+            squareLength = getInput();
+            Ex01_02.Program.printTiltedSquare(1,squareLength);
+            Console.ReadLine();
+        }
+
+        private static int getInput()
+        {
+            bool v_isValidInput = false;
+            int inputNumber = 0;
+            string inputString;
+
+            while(v_isValidInput == false)
+            {
+                Console.WriteLine("Please enter VALID Meuyan height:");
+                inputString = Console.ReadLine();
+                v_isValidInput = int.TryParse(inputString, out inputNumber);
+                if(v_isValidInput && inputNumber<0)
+                {
+                    v_isValidInput = false;
+                }
+            }
+
+            if(inputNumber % 2 == 0)
+            {
+                inputNumber++;
+            }
+
+            return inputNumber;
+        }
+    }
+}
